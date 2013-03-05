700abe5c99298fd3959737f5cd7ea0df2fa25632
Matt Forster <forster.matth@gmail.com>
2013-03-05 13:54:11 -0700

    linebreaks

--- site/common/contact.php
+++ site/common/contact.php
@@ -12,10 +12,10 @@
 				<span class="lead">Innovation, Quality and Style.</span> <br>
 				Creating cool things, solving problems as well as maintaining good code keeps me happy and motivated.
 				I am interested in web development, application development, and other projects that allow me to create and 
-				help people.
+				help people.<br><br>
 
 				I've dreamed of working for NASA, and I've made it a goal in my life to help the human race 
-				move forward in some significant way.
+				move forward in some significant way. <br><br>
 
 				Just Finishing my Computer Information Technology diploma at the Lethbridge College, I am planning on attending the
 				University of Lethbridge and finishing my degree.
