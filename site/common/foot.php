584621099f6267b7060b34c96d73aee60f85139d
Matt Forster <forster.matth@gmail.com>
2013-03-26 01:07:08 -0600

    change

--- site/common/foot.php
+++ site/common/foot.php
@@ -6,7 +6,7 @@
 			<p>
 				<small>
 					Styled with <a target="_blank" href="http://twitter.github.com/bootstrap/">Twitter Bootstrap.</a> 
-					The rest was imagineered by <a target="_blank" href="#">Matt Forster.</a>
+					The rest was imagineered by <a href="#">Matt Forster.</a>
 					Source code hosted on <a target="_blank" href="https://github.com/forstermatth/Portfolio">GitHub.</a>
 				</small>
 			</p>
