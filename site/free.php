73973a2c0cffe55ccfb1b534bafd122e8cb95714
Matt Forster <forster.matth@gmail.com>
2013-04-01 14:52:08 -0600

    img bug

--- site/free.php
+++ site/free.php
@@ -10,7 +10,7 @@
 	  <!-- Carousel items -- -->
 	  <div class="carousel-inner">
 		<div class="active item">
-			<img src="../img/Skills.jpg" alt="valley">
+			<img src="../img/skills.jpg" alt="valley">
 			<div class="carousel-caption">
 				<p class="lead"><span class="highlight3">Freelancing</span></p>
 				<p><small>Looking to hire me? Read on!</small></p>
