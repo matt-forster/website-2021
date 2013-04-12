6ee00531fd9709ade6a23ce78711e50089a4e492
Matt Forster <forster.matth@gmail.com>
2013-04-11 19:57:55 -0600

    bugs

--- site/common/foot.php
+++ site/common/foot.php
@@ -19,7 +19,7 @@
 </div> <!-- /container -->
 		
 <!-- Scripts -------------------------------------------------------------- -->
-		<script src="../js/vendor/bootstrap.min.js"></script>
+		<script src="../js/vendor/bootstrap.js"></script>
         
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
