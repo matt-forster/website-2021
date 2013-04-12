fe1a3791545609e95823eea3057debda70a292a6
Matt Forster <forster.matth@gmail.com>
2013-04-12 14:40:08 -0600

    regular .css files

--- site/common/top.php
+++ site/common/top.php
@@ -11,8 +11,8 @@
     <meta name="description" content="Matt Forster's Portofolio!">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="../css/vendor/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/vendor/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="../css/vendor/bootstrap.css">
+    <link rel="stylesheet" href="../css/vendor/bootstrap-responsive.css">
     <link href="../css/vendor/hint.css" 	  rel="stylesheet">
  	<link href="../css/custom.css"    rel="stylesheet">
 
