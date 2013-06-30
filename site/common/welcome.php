0b9c3320616dd3b16ce08e539fea5ff28cf37b8b
Matt Forster <forster.matth@gmail.com>
2013-06-30 08:23:34 -0600

    phone boxes

--- site/common/welcome.php
+++ site/common/welcome.php
@@ -15,45 +15,51 @@
 		if($page == "current"){
 			echo 
 				'<div class="row center">
-					<div class="span4 feature highlight1" id="link1">
-						<p>Current Projects</p>
-					</div>
-					<div class="span4 feature mouse-over" id="link2">
-						<p><a href="past.php">Past<br> Projects</a></p>
-					</div>
-					<div class="span4 feature mouse-over" id="link3">
-						<p><a href="free.php">Freelancing</a>  <span class="smaller muted"> Looking to hire me?</span></p>
-					</div>
+                    <div class="well hidden-desktop">
+                        <div class="span4 feature highlight1 " id="link1">
+                            <p>Current Projects</p>
+                        </div>
+                        <div class="span4 feature mouse-over" id="link2">
+                            <p><a href="past.php">Past<br> Projects</a></p>
+                        </div>
+                        <div class="span4 feature mouse-over" id="link3">
+                            <p><a href="free.php">Freelancing</a>  <span class="smaller muted"> Looking to hire me?</span></p>
+                        </div>
+                    </div>
 				</div>'
 			;
 		}
 		if($page == "past"){
 			echo 
 				'<div class="row center">
-					<div class="span4 feature mouse-over" id="link1">
-						<p><a href="current.php">Current Projects</a></p>
-					</div>
-					<div class="span4 feature highlight2" id="link2">
-						<p>Past<br> Projects</p>
-					</div>
-					<div class="span4 feature mouse-over" id="link3">
-						<p><a href="free.php">Freelancing</a> <span class="smaller muted"> Looking to hire me?</span></p>
-					</div>
+                    <div class="well hidden-desktop">
+                        <div class="span4 feature mouse-over" id="link1">
+                            <p><a href="current.php">Current Projects</a></p>
+                        </div>
+                        <div class="span4 feature highlight2" id="link2">
+                            <p>Past<br> Projects</p>
+                        </div>
+                        <div class="span4 feature mouse-over" id="link3">
+                            <p><a href="free.php">Freelancing</a> <span class="smaller muted"> Looking to hire me?</span></p>
+                        </div>
+                    </div>
 				</div>'
 			;
 		}
 		if($page == "free"){
 			echo 
 				'<div class="row center">
-					<div class="span4 feature mouse-over" id="link1">
-						<p><a href="current.php">Current Projects</a></p>
-					</div>
-					<div class="span4 feature mouse-over" id="link2">
-						<p><a href="past.php">Past<br> Projects</a></p>
-					</div>
-					<div class="span4 feature highlight3" id="link3">
-						<p>Freelancing  <span class="smaller muted"> Looking to hire me?</span></p>
-					</div>
+                    <div class="well hidden-desktop">
+                        <div class="span4 feature mouse-over" id="link1">
+                            <p><a href="current.php">Current Projects</a></p>
+                        </div>
+                        <div class="span4 feature mouse-over" id="link2">
+                            <p><a href="past.php">Past<br> Projects</a></p>
+                        </div>
+                        <div class="span4 feature highlight3" id="link3">
+                            <p>Freelancing  <span class="smaller muted"> Looking to hire me?</span></p>
+                        </div>
+                    </div>
 				</div>'
 			;
 		}
